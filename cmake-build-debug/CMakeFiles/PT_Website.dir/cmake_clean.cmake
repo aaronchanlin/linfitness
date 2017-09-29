aa649a8cc2b4aef1bee8c0269429e40c62cae005
aaronlin <aaronlin@Aarons-MacBook-Air.local>
2017-09-29 17:04:05 -0400

    website testing
    testing

---
+++ cmake-build-debug/CMakeFiles/PT_Website.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/PT_Website.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PT_Website.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PT_Website.
+# This may be replaced when dependencies are built.
